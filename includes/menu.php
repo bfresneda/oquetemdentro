bf34b49047b1243089a29694a7f77fe2598597e9
Bruno Fresneda Cezero <bruno.fresneda@gmail.com>
2019-12-13 15:14:16 -0300

    inicio projeto

---
+++ includes/menu.php
@@ -0,0 +1,19 @@
+
+
+    <link rel="stylesheet" href="./style.css">
+        <nav id="wrapper">
+            <div id="header" class="">
+                <div id="logo">
+                    <h1><a href="#">O que tem dentro?</a></h1>
+                </div>
+                <div id="menu" class="bg-light p-4 w-100 d-flex justify-content-center">
+                    <ul>
+                        <li class="current_page_item"><a href="./index.php" accesskey="1" title="">Home</a></li>
+                        <li><a href="#" accesskey="2" title="">Consultar produtos</a></li>
+                        <li><a href="./cad_produtos.php" accesskey="3" title="">Cadastrar produtos</a></li>
+                        <li><a href="#" accesskey="4" title="">Cadastrar alergenos</a></li>
+                        <li><a href="./cadastro.php" accesskey="5" title="">Cadastre-se</a></li>
+                        <li class="Login"><a href="./login.php" accesskey="5" title="">Login </a></li>
+                    </ul>
+                </div>
+            </nav>
